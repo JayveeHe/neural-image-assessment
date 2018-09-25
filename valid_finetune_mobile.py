5c52695a43734a7c2620730e1ab74fc7c77dc442
Jiawei <hejiawei03@meituan.com>
2018-09-25 15:42:22 +0800

    add validation

--- valid_finetune_mobile.py
+++ valid_finetune_mobile.py
@@ -56,7 +56,7 @@ elif args.img[0] is not None:
 else:
     raise RuntimeError('Either -dir or -img arguments must be passed as argument')
 
-with tf.device('/CPU:0'), open(args.output, 'w') as fout:
+with  open(args.output, 'w') as fout:
     base_model = MobileNet((None, None, 3), alpha=1, include_top=False, pooling='avg', weights=None)
     x = Dropout(0.75)(base_model.output)
     x = Dense(10, activation='softmax')(x)
